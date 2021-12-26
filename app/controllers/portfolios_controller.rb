0709cf0de844b0d7e636214b791bccab80151a28
eimoh17 <eimohmohaunglwin@gmail.com>
2021-12-26 23:38:28 +0630

    Refactored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,7 +8,7 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.new
 	end
 	def create
-    @portfolio_item = Portfolio.new( params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -29,7 +29,7 @@ class PortfoliosController < ApplicationController
   	@portfolio_item = Portfolio.find(params[:id])
   
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: "Your portfolio  was successfully updated." }
         
       else
@@ -54,6 +54,12 @@ def destroy
       format.html { redirect_to blogs_url, notice: "Record was removed." }
       format.json { head :no_content }
     end
+  end
+
+  private
+  def portfolio_params
+   params.require(:portfolio).permit(:title, :subtitle, :body)
+    
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ Bundler.require(*Rails.groups)
 
 module DevCamp
   class Application < Rails::Application
+
+    config.action_controller.permit_all_parameters = true
+
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
